836364d3fabeeb7d4586d43999c7ea95982112ce
Chih-Wei Ning <b04902079@ntu.edu.tw>
2017-12-10 12:08:33 +0800

    Add 2 socket events still some bugs: buttons do not reset to null

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-// var socket = io.connect();
-
-// recieve eye-tracker position
-
-var eyeX, eyeY;
+var socket = io.connect();
 var button_left, button_right, button_up, button_down;
 
+// recieve eye-tracker position
 $(document).mousemove( function(e) {
+	changePos(e.pageX, e.pageY);
+});
 
-	eyeX = e.pageX;	eyeY = e.pageY;
+socket.on('eyemove', function(x, y){
+	changePos(x, y);
+});
+
+socket.on('swipe', function(dir){
+	var target;
+
+	if( dir == 'up' ) target = button_up;
+	if( dir == 'down' ) target = button_down;
+	if( dir == 'left' ) target = button_left;
+	if( dir == 'right' ) target = button_right;
+
+	if( target != null ) target.click();
+});
+
+function changePos (eyeX, eyeY) {
 
 	$('#eye_tracker').css({
 		"left": eyeX,
@@ -23,20 +37,20 @@ $(document).mousemove( function(e) {
 			var dist = ( btnX - eyeX )*( btnX - eyeX ) + ( btnY - eyeY )*( btnY - eyeY );
 			if ( dist <= 64000 ) {
 				btn.find('img').show();
+				if( i%2 == 0 && j%2 == 0 ) button_up = btn;
 				if( i%2 == 1 && j%2 == 1 ) button_down = btn;
 				if( i%2 == 1 && j%2 == 0 ) button_left = btn;
 				if( i%2 == 0 && j%2 == 1 ) button_right = btn;
-				if( i%2 == 0 && j%2 == 0 ) button_up = btn;
 			}
-			else {
+			else { 
 				btn.find('img').hide();
-				if( i%2 == 1 && j%2 == 1 ) button_down = null;
-				if( i%2 == 1 && j%2 == 0 ) button_left = null;
-				if( i%2 == 0 && j%2 == 1 ) button_right = null;
-				if( i%2 == 0 && j%2 == 0 ) button_up = null;
+				if( i%2 == 0 && j%2 == 0 && btn == button_up ) button_up = null;
+				if( i%2 == 1 && j%2 == 1 && btn == button_down ) button_down = null;
+				if( i%2 == 1 && j%2 == 0 && btn == button_left ) button_left = null;
+				if( i%2 == 0 && j%2 == 1 && btn == button_right ) button_right = null;
 			}
 		}
-});
+}
 
 // recieve swiping event
 
