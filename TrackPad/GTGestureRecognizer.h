07050d4a64aa96f9542a218e3cd49a00098bbc28
Tony Tung <tony61507@gmail.com>
2017-12-10 16:49:24 -0800

    added swipe recognizer

--- TrackPad/GTGestureRecognizer.h
+++ TrackPad/GTGestureRecognizer.h
@@ -7,7 +7,21 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import "TouchPoint.h"
+#import "Utils.h"
 @interface GTGestureRecognizer : NSObject
 
+//typedef enum RecognitionState : NSInteger
+//{
+//    NO_TOUCH_STATE,
+//    START_STATE,
+//    CHANGE_STATE,
+//    TIMEOUT_STATE,
+//    END_STATE
+//} RecognitionState;
+@property GestureMode gestureMode;
+@property float timeThreshold;
+@property NSMutableArray *points;
+//@property enum RecognitionState currentState;
+-(SwipeGesture)recognizeGesture:(NSMutableArray *)touchList;
 @end
--- TrackPad/GTGestureRecognizer.m
+++ TrackPad/GTGestureRecognizer.m
@@ -9,5 +9,47 @@
 #import "GTGestureRecognizer.h"
 
 @implementation GTGestureRecognizer
+@synthesize timeThreshold;
+@synthesize gestureMode;
+
+-(GTGestureRecognizer *)init {
+    self = [super init];
+    timeThreshold = 0.2f;
+    gestureMode = SWIPE_GESTURE;
+    return self;
+}
+
+-(SwipeGesture)recognizeGesture:(NSMutableArray *) touchList {
+    SwipeGesture gesture = UNKOWN;
+    if([touchList count] == 0)
+        return gesture;
+    float dX = [touchList[[touchList count] - 1] x] - [touchList[0] x];
+    float dY = [touchList[[touchList count] - 1] y] - [touchList[0] y];
+    NSLog(@"dX: %f, dY: %f", dX, dY);
+    if(fabs(dX) - fabs(dY) >= 0.1) {
+        // horizontal swipe
+        if(dX > 0){
+            //swipe right
+            gesture = SWIPE_RIGHT;
+        } else {
+            //swipe left
+            gesture = SWIPE_LEFT;
+        }
+    } else if(fabs(dX) - fabs(dY) <= -0.1){
+        //vertical swipe
+        if(dY > 0){
+            //swipe up
+            gesture = SWIPE_UP;
+        } else {
+            //swipe down
+            gesture = SWIPE_DOWN;
+        }
+    }
+    return gesture;
+}
+
+
+
+
 
 @end
