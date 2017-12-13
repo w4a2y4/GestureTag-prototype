72386cfd39ea8f406dfcd558e93b28e0aaf610a9
Tony Tung <tony61507@gmail.com>
2017-12-12 21:43:32 -0800

    tested webgazer by using frontend callback instead of socket

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var socket = io.connect();
+var socket = io();
 var button_left, button_right, button_up, button_down;
 var show_left, show_right, show_up, show_down;
 
@@ -22,7 +22,7 @@ function changePos (eyeX, eyeY) {
 
 	$('#eye_tracker').css({
 		"left": eyeX,
-		"top": eyeY 
+		"top": eyeY
 	});
 
 	show_up = false;
@@ -30,7 +30,7 @@ function changePos (eyeX, eyeY) {
 	show_left = false;
 	show_right = false;
 
-	for( var i=0; i<3; i++ ) 
+	for( var i=0; i<3; i++ )
 		for( var j=0; j<4; j++) {
 			var btn = $("#blk"+i+""+j+" button");
 			var btnX = btn.offset().left + 100;
@@ -48,6 +48,27 @@ function changePos (eyeX, eyeY) {
 		}
 }
 
+// window.onload = function() {
+// 	webgazer.setRegression('ridge') /* currently must set regression and tracker */
+// 		.setTracker('clmtrackr')
+// 		.setGazeListener(function(data, clock) {
+// 			if(data === null)
+// 			  return;
+// 			// console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
+// 		 //   console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
+// 			emitEyeMove(data);
+// 		})
+// 		.begin()
+// 		.showPredictionPoints(true);
+// };
+// var emitEyeMove = (data) => {
+// 	// socket.emit('eyemove', data.x, data.y);
+// 	// changePos(data.x, data.y);
+// }
+// window.onbeforeunload = function() {
+// 	//webgazer.end(); //Uncomment if you want to save the data even if you reload the page.
+// 	window.localStorage.clear(); //Comment out if you want to save data across different sessions
+// }
 // recieve swiping event
 
 $(document).on('click', 'button', ( function(e) {
