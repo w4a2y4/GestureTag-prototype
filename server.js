182efac0b1437ac82eb94163c5c4a80bb3436c8b
Tony Tung <tony61507@gmail.com>
2017-12-17 02:22:30 -0800

    used render engine to handle tap and arrow images

--- server.js
+++ server.js
@@ -2,12 +2,42 @@ const fs = require('fs');
 const moment = require('moment');
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname));
+const path = require('path');
+const resources = '/resources';
+const swipeImages = {
+    OPTION_1: `${resources}/arrow_up.png`,
+    OPTION_2: `${resources}/arrow_down.png`,
+    OPTION_3: `${resources}/arrow_left.png`,
+    OPTION_4: `${resources}/arrow_right.png`
+};
+const tapImages = {
+    OPTION_1: `${resources}/tap_topright.png`,
+    OPTION_2: `${resources}/tap_bottomleft.png`,
+    OPTION_3: `${resources}/tap_topleft.png`,
+    OPTION_4: `${resources}/tap_bottomright.png`
+};
+const type = process.argv[2];
+app.use(resources, express.static('resources'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    res.render('index', loadImages());
+});
+
+app.get('/swipe', (req, res) => {
+    res.sendFile(path.join(__dirname, 'swipe.html'));
+    // res.render('swipe');
+});
+
+app.get('/tap', (req, res) => {
+    res.sendFile(path.join(__dirname, 'tap.html'));
+    // res.render('tap');
+});
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-function writeLog ( msg ) {
+var writeLog = ( msg ) => {
     var filename = 'tmp.log';
     var time = moment().format('MM/DD hh:mm:ss:SSS');
     msg = time + '\t' + msg + '\n';
@@ -15,6 +45,13 @@ function writeLog ( msg ) {
     fs.appendFile( filename , msg, function(err){
         if (err) console.error(err);
     });
+};
+
+var loadImages = () => {
+    if(type === 'swipe')
+        return swipeImages;
+    else if (type === 'tap')
+        return tapImages;
 }
 
 io.on('connection', function(socket){
@@ -26,9 +63,19 @@ io.on('connection', function(socket){
     });
 
     // recieve swiping event
-    socket.on('swipe', function(dir){
-        io.emit('swipe', dir);
-    });
+    if(type === 'swipe'){
+        socket.on('swipe', function(dir){
+            io.emit('swipe', dir);
+        });
+    }
+
+    //recieve tap event
+    if(type === 'tap'){
+        socket.on('tap', (location) => {
+            console.log(`location: ${location}`);
+            io.emit('tap', location);
+        });
+    }
 
     // start a trial
     socket.on('start', function(){
@@ -44,7 +91,7 @@ io.on('connection', function(socket){
 
     socket.on('log', function(cnt, gesture, clicked_btn, target){
         var msg = '#' + cnt;
-        msg += '\tswipe:' + gesture;
+        msg += '\tevent:' + gesture;
         msg += '\ttarget:' + target;
         msg += '\tclick:' + clicked_btn;
         writeLog(msg);
