c833812d3fe631e5094b78cb3da4e29984635b4c
Oliveck10 <oliveck10@gmail.com>
2018-01-17 21:37:09 +0800

    change swipe to eight direction

--- resources/swipe.js
+++ resources/swipe.js
@@ -6,29 +6,32 @@ manager.add(swipe);
 
 manager.on('swipe', (e) => {
     var direction = e.offsetDirection;
-    var directionStr = getSwipeDirectionStr(direction);
-    emitSwipeGesture(directionStr);
-    e.target.innerText = directionStr;
+    var angle = e.angle;
+    const dirStr = getSwipeDirectionFromAngle(angle);
+    // console.log(`${dirStr}, ${angle}`);
+    emitSwipeGesture(dirStr);
+    e.target.innerText = `${dirStr}`;
 });
 
-var getSwipeDirectionStr = (direction) => {
-    var gesture = '';
-    switch (direction) {
-        case Hammer.DIRECTION_LEFT:
-            gesture = 'left';
-            break;
-        case Hammer.DIRECTION_RIGHT:
-            gesture = 'right';
-            break;
-        case Hammer.DIRECTION_UP:
-            gesture = 'up'
-            break;
-        case Hammer.DIRECTION_DOWN:
-            gesture = 'down';
-            break;
-        default:
-            gesture = 'unknown';
-            break;
+/* right : 0, up: -90, left: 180, down: 90 */
+var getSwipeDirectionFromAngle = (angle) => {
+    let dir = '';
+    if (angle < 22.5 && angle >= -22.5) {
+        dir = 'right';
+    } else if (angle < -22.5 && angle >= -67.5) {
+        dir = 'upright';
+    } else if (angle < -67.5 && angle >= -112.5) {
+        dir = 'up';
+    } else if (angle < -112.5 && angle >= -157.5) {
+        dir = 'upleft';
+    } else if (angle < -157.5 || angle > 157.5) {
+        dir = 'left';
+    } else if (angle > 112.5 && angle <= 157.5) {
+        dir = 'downleft';
+    } else if (angle > 67.5 && angle <= 112.5) {
+        dir = 'down';
+    } else {
+        dir = 'downright';
     }
-    return gesture;
-}
+    return dir;
+};
