27faa12b6f8c8d19cb614302fac499414b57d4d9
Oliveck10 <oliveck10@gmail.com>
2018-01-18 13:23:39 +0800

    add touch event handler

--- resources/log.js
+++ resources/log.js
@@ -1,10 +1,13 @@
-var button = document.querySelector("button");
 const socket = io();
 
 var emitSwipeGesture = (gesture) => {
     socket.emit('swipe', gesture);
-}
+};
 
 var emitTapGesture = (quadrant) => {
     socket.emit('tap', quadrant);
-}
+};
+
+var emitTouchData = (event) => {
+    socket.emit('touch', event);
+};
