d7fbfa64a9be58d0a4fbf23ba77acec985d06d63
Oliveck10 <oliveck10@gmail.com>
2018-01-25 17:57:42 +0800

    divide the users to different gesture(swipe) set

--- resources/swipe.js
+++ resources/swipe.js
@@ -1,13 +1,18 @@
 var container = document.querySelector(".container");
 const manager = new Hammer.Manager(container);
 const swipe = new Hammer.Swipe();
-
+var tester;
 manager.add(swipe);
+
+socket.on('user', function(user) {
+    tester = user;
+    console.log(tester);
+});
 
 manager.on('swipe', (e) => {
     var direction = e.offsetDirection;
     var angle = e.angle;
-    const dirStr = getSwipeDirectionFromAngle(angle);
+    const dirStr = getSwipeDirectionFromAngle(angle, direction);
     // console.log(`${dirStr}, ${angle}`);
     emitSwipeGesture(dirStr);
     e.target.innerText = `${dirStr}`;
@@ -29,24 +34,37 @@ manager.on('hammer.input', (ev) => {
 });
 
 /* right : 0, up: -90, left: 180, down: 90 */
-var getSwipeDirectionFromAngle = (angle) => {
+var getSwipeDirectionFromAngle = (angle, direction) => {
     let dir = '';
-    if (angle < 22.5 && angle >= -22.5) {
-        dir = 'right';
-    } else if (angle < -22.5 && angle >= -67.5) {
-        dir = 'upright';
-    } else if (angle < -67.5 && angle >= -112.5) {
-        dir = 'up';
-    } else if (angle < -112.5 && angle >= -157.5) {
-        dir = 'upleft';
-    } else if (angle < -157.5 || angle > 157.5) {
-        dir = 'left';
-    } else if (angle > 112.5 && angle <= 157.5) {
-        dir = 'downleft';
-    } else if (angle > 67.5 && angle <= 112.5) {
-        dir = 'down';
+    if (tester === 'motor') {
+        console.log('motor tester');
+        if (direction === Hammer.DIRECTION_RIGHT) {
+            dir = 'right';
+        } else if (direction === Hammer.DIRECTION_UP) {
+            dir = 'up';
+        } else if (direction === Hammer.DIRECTION_LEFT) {
+            dir = 'left';
+        } else if (direction === Hammer.DIRECTION_DOWN) {
+            dir = 'down';
+        }
     } else {
-        dir = 'downright';
+        if (angle < 22.5 && angle >= -22.5) {
+            dir = 'right';
+        } else if (angle < -22.5 && angle >= -67.5) {
+            dir = 'upright';
+        } else if (angle < -67.5 && angle >= -112.5) {
+            dir = 'up';
+        } else if (angle < -112.5 && angle >= -157.5) {
+            dir = 'upleft';
+        } else if (angle < -157.5 || angle > 157.5) {
+            dir = 'left';
+        } else if (angle > 112.5 && angle <= 157.5) {
+            dir = 'downleft';
+        } else if (angle > 67.5 && angle <= 112.5) {
+            dir = 'down';
+        } else {
+            dir = 'downright';
+        }
     }
     return dir;
 };
