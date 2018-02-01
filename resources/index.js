da4a8dd9ea43ee2229133bb7d6b6a784d671d858
aldrich1221 <30518471+aldrich1221@users.noreply.github.com>
2018-02-01 23:32:19 +0800

    mobile sys

--- resources/index.js
+++ resources/index.js
@@ -1,9 +1,16 @@
+
+var container = document.getElementById("MobileContainer")
+const manager = new Hammer.Manager(container);
+const swipe = new Hammer.Swipe();
+var tester;
+manager.add(swipe);
+
 var socket = io.connect();
 var show_path = false;
 var show_mouse = false;
 
 var button_left, button_right, button_up, button_down, button_upright, button_downright, button_downleft, button_upleft;
-var show_left, show_right, show_up, show_down, show_upright, show_downright, show_downleft, show_downright;
+var show_left, show_right, show_up, show_down, show_upright, show_upleft, show_downleft, show_downright;
 var type;
 
 const DEFAULT_TRIAL_NUM = 12;
@@ -303,3 +310,60 @@ function clearCanvas() {
     cxt.beginPath();
     new_path = true;
 }
+
+
+
+manager.on('swipe', (e) => {
+    var direction = e.offsetDirection;
+    var angle = e.angle;
+    const dirStr = getSwipeDirectionFromAngle(angle, direction);
+    //console.log(`${dirStr}, ${angle}`);
+    
+    
+    e.target.innerText = `${dirStr}`;
+    if (dirStr == 'up' && show_up) button_up.click();
+    if (dirStr == 'down' && show_down) button_down.click();
+    if (dirStr == 'left' && show_left) button_left.click();
+    if (dirStr == 'right' && show_right) button_right.click();
+    if (dirStr == 'upright' && show_upright) button_upright.click();
+    if (dirStr == 'downright' && show_downright) button_downright.click();
+    if (dirStr == 'downleft' && show_downleft) button_downleft.click();
+    if (dirStr == 'upleft' && show_upleft) button_upleft.click();
+
+
+});
+
+var getSwipeDirectionFromAngle = (angle, direction) => {
+    let dir = '';
+    if (tester === 'motor') {
+        console.log('motor tester');
+        if (direction === Hammer.DIRECTION_RIGHT) {
+            dir = 'right';
+        } else if (direction === Hammer.DIRECTION_UP) {
+            dir = 'up';
+        } else if (direction === Hammer.DIRECTION_LEFT) {
+            dir = 'left';
+        } else if (direction === Hammer.DIRECTION_DOWN) {
+            dir = 'down';
+        }
+    } else {
+        if (angle < 22.5 && angle >= -22.5) {
+            dir = 'right';
+        } else if (angle < -22.5 && angle >= -67.5) {
+            dir = 'upright';
+        } else if (angle < -67.5 && angle >= -112.5) {
+            dir = 'up';
+        } else if (angle < -112.5 && angle >= -157.5) {
+            dir = 'upleft';
+        } else if (angle < -157.5 || angle > 157.5) {
+            dir = 'left';
+        } else if (angle > 112.5 && angle <= 157.5) {
+            dir = 'downleft';
+        } else if (angle > 67.5 && angle <= 112.5) {
+            dir = 'down';
+        } else {
+            dir = 'downright';
+        }
+    }
+    return dir;
+};
