2ca25b49811dc31f549738c2d69e6978a7f24417
Oliveck10 <oliveck10@gmail.com>
2018-02-21 13:05:46 +0800

    multi-finger-swipe for motor (desktop version)

--- resources/swipe.js
+++ resources/swipe.js
@@ -16,15 +16,6 @@ socket.on('init', function(method) {
     socket.emit('client_init', width, height);
 });
 
-manager.on('swipe', (e) => {
-    var direction = e.offsetDirection;
-    var angle = e.angle;
-    const dirStr = getSwipeDirectionFromAngle(angle, direction);
-    // console.log(`${dirStr}, ${angle}`);
-    emitSwipeGesture(dirStr);
-    e.target.innerText = `${dirStr}`;
-});
-
 /* send touch raw data */
 manager.on('hammer.input', (ev) => {
     const touch = {
@@ -34,10 +25,38 @@ manager.on('hammer.input', (ev) => {
             y: ev.center.y
         }
     };
-
     emitTouchData(touch);
     console.log(touch.pos)
-        // console.log(ev.type);
+
+    console.log(ev);
+    // If one can only do multi-touch swipe
+    if (ev.maxPointers > 1) {
+        if (ev.isFinal === true) {
+            let multidir = ev.direction
+            if (multidir === Hammer.DIRECTION_RIGHT) {
+                dir = 'right';
+            } else if (multidir === Hammer.DIRECTION_UP) {
+                dir = 'up';
+            } else if (multidir === Hammer.DIRECTION_LEFT) {
+                dir = 'left';
+            } else if (multidir === Hammer.DIRECTION_DOWN) {
+                dir = 'down';
+            }
+            emitSwipeGesture(dir);
+            console.log("M: " + dir);
+            ev.target.innerText = `${dir}`
+        }
+    }
+});
+
+// If one can do single-touch swipe
+manager.on('swipe', (e) => {
+    var direction = e.offsetDirection;
+    var angle = e.angle;
+    const dirStr = getSwipeDirectionFromAngle(angle, direction);
+    // console.log(`${dirStr}, ${angle}`);
+    emitSwipeGesture(dirStr);
+    e.target.innerText = `${dirStr}`;
 });
 
 /* right : 0, up: -90, left: 180, down: 90 */
@@ -73,5 +92,6 @@ var getSwipeDirectionFromAngle = (angle, direction) => {
             dir = 'downright';
         }
     }
+    console.log("A: " + dir)
     return dir;
 };
