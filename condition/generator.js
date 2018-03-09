73927561e551612f2136ee7a53bde481af57fa9b
Oliveck10 <oliveck10@gmail.com>
2018-03-09 19:12:44 +0800

    adjust json config

--- condition/generator.js
+++ condition/generator.js
@@ -4,7 +4,7 @@ let savedSequences = [];
 
 
 var shuffle = (arr) => {
-    for(let i = arr.length - 1; i > 0; i--){
+    for (let i = arr.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [arr[i], arr[j]] = [arr[j], arr[i]];
     }
@@ -12,18 +12,26 @@ var shuffle = (arr) => {
 };
 
 var generateSequence = () => {
-    let order = [[16, 0], [16, 0.5], [16, 1],
-                 [32, 0], [32, 0.5], [32, 1],
-                 [48, 0], [48, 0.5], [48, 1]];
+    let order = [
+        [16, 0],
+        [16, 0.5],
+        [16, 1],
+        [32, 0],
+        [32, 0.5],
+        [32, 1],
+        [48, 0],
+        [48, 0.5],
+        [48, 1]
+    ];
     return shuffle(order);
 };
 
 var generateConditionFiles = (location, max_id) => {
-    for(let i = 0; i < max_id; i++){
+    for (let i = 0; i < max_id; i++) {
         let userSeqs = [];
-        for(let j = 0; j < 3; j++){
+        for (let j = 0; j < 3; j++) {
             let seq = generateSequence();
-            while(isDuplicated(seq)){
+            while (isDuplicated(seq)) {
                 seq = generateSequence();
             }
             savedSequences.push(seq);
@@ -34,8 +42,8 @@ var generateConditionFiles = (location, max_id) => {
 };
 
 var isDuplicated = (currentSeq) => {
-    for(let i = 0; i < savedSequences.length; i++){
-        if(JSON.stringify(currentSeq) === JSON.stringify(savedSequences[i]))
+    for (let i = 0; i < savedSequences.length; i++) {
+        if (JSON.stringify(currentSeq) === JSON.stringify(savedSequences[i]))
             return true;
     }
     return false;
@@ -49,7 +57,7 @@ var saveFile = (location, id, data) => {
         dwell: data[2]
     };
     fs.writeFile(`./${userConditions.id}.json`, JSON.stringify(userConditions, null, 4), (err) => {
-        if(err){
+        if (err) {
             console.log('Error at writing a file.');
             return;
         }
