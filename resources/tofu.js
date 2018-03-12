ffa89cec036f5f37c393a00df749e108b3a06999
Oliveck10 <oliveck10@gmail.com>
2018-03-12 20:40:20 +0800

    implement Pearson correlation coefficient

--- resources/tofu.js
+++ resources/tofu.js
@@ -891,3 +891,49 @@ function DwellLockerReset(eyeX, eyeY) {
         }
     }
 }
+
+function getPearsonCorrelation(x, y) {
+    var arrLength = 0;
+
+    if (x.length == y.length) {
+        arrLength = x.length;
+    } else if (x.length > y.length) {
+        arrLength = y.length;
+        console.error('x has more items in it, the last ' + (x.length - arrLength) + ' item(s) will be ignored');
+    } else {
+        arrLength = x.length;
+        console.error('y has more items in it, the last ' + (y.length - arrLength) + ' item(s) will be ignored');
+    }
+
+    var xy = [];
+    var x2 = [];
+    var y2 = [];
+
+    for (var i = 0; i < arrLength; i++) {
+        xy.push(x[i] * y[i]);
+        x2.push(x[i] * x[i]);
+        y2.push(y[i] * y[i]);
+    }
+
+    var sum_x = 0;
+    var sum_y = 0;
+    var sum_xy = 0;
+    var sum_x2 = 0;
+    var sum_y2 = 0;
+
+    for (var i = 0; i < arrLength; i++) {
+        sum_x += x[i];
+        sum_y += y[i];
+        sum_xy += xy[i];
+        sum_x2 += x2[i];
+        sum_y2 += y2[i];
+    }
+
+    var step1 = (arrLength * sum_xy) - (sum_x * sum_y);
+    var step2 = (arrLength * sum_x2) - (sum_x * sum_x);
+    var step3 = (arrLength * sum_y2) - (sum_y * sum_y);
+    var step4 = Math.sqrt(step2 * step3);
+    var answer = step1 / step4;
+
+    return answer;
+}
